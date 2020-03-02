6ddeee1abfccd9124fed1f4779926f1a57a9c342
suntt2019 <suntiantian@emails.bjut.edu.cn>
2020-03-03 06:35:40 +0800

    try demo1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include <easyx.h>
-#include <stdio.h>
+#include<graphics.h>
+#include <conio.h>
 
-int main() {
-	printf("yo.\n");
+#define WIDTH 640
+#define HEIGHT 480
+
+int main()
+{
+	IMAGE back;
+	IMAGE p1;
+	IMAGE p2;
+	int i;
+
+	initgraph(WIDTH, HEIGHT);
+
+	loadimage(&back, "image\\background.jpg");
+	loadimage(&p1, "image\\fish02.jpg");
+	loadimage(&p2, "image\\fish01.jpg");
+	i = 10;
+	while (1) {
+		BeginBatchDraw();//开始批量绘图
+		putimage(0, 0, &back);
+		//	putimage(i, 100, &p2);
+		putimage(i, 100, &p1, SRCAND);
+		putimage(i, 100, &p2, SRCINVERT);
+		EndBatchDraw();//结束批量绘图，将绘制好的图片统一贴到屏幕上。	
+		Sleep(12);	//暂停
+		i++;
+		if (i > WIDTH - 10) i = 10;
+	}
+
 	return 0;
 }
